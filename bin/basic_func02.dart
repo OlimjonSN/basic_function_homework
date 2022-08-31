39e9201868c5d1ea0730f46f7240df78913dd083
Olimjon <olimjonsn230@gmail.com>
2022-08-31 10:24:12 +0500

    task two done

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,10 @@
-// Create a function called divide. 
-//Create function arguments a and b of type int. 
+// Create a function called func.
+//Create function arguments a and b of type int.
 //Returns the floor division of a and b.
-void main() {}
+void main() {
+  print(func(5, 3));
+}
+
+int func(a, b) {
+  return a ~/ b;
+}
