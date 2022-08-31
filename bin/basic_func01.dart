ce12a4e5477c9a56094b53d591c5603db16bac38
Olimjon <olimjonsn230@gmail.com>
2022-08-31 10:20:41 +0500

    task one done

--- bin/basic_func01.dart
+++ bin/basic_func01.dart
@@ -1,4 +1,10 @@
 // Create a function called func.
 // Create two arguments type int
 //  Return the summ two arguments
-void main() {}
+void main() {
+  print(func(8, 2));
+}
+
+int func(int x, int y) {
+  return x + y;
+}
